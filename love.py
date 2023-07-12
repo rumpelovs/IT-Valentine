8f0c18246c2003e45c5f6ff7c53a8f02ab25ccc2
rumpelovs <rumpelstilchen62@gmail.com>
2023-07-12 20:09:29 +0300

    Update love.py

--- love.py
+++ love.py
@@ -1,5 +1,6 @@
 import time
-from colorama import init, Fore
+import turtle
+import math
 
 import time
 
@@ -13,18 +14,18 @@ def love_confession():
     message2 = " Ты - самая прекрасная и удивительная девушка, которую я когда-либо встречал."
     for char in message2:
         print(char, end='', flush=True)
-        time.sleep(0.06)
+        time.sleep(0.04)
    
 
     message3 = " Твоя улыбка заставляет мое сердце биться быстрее, а твой голос звучит как мелодия."
-    for char in message2:
+    for char in message3:
         print(char, end='', flush=True)
-        time.sleep(0.06)
+        time.sleep(0.04)
     
     message4 = " Когда я вижу тебя, моя душа наполняется радостью, а мои мысли о тебе заполняют каждый уголок моего разума. Ты - воплощение красоты и нежности, и я не могу удержаться от того, чтобы не сказать тебе правду - ты безумно мне нравишься!"
     for char in message4:
         print(char, end='', flush=True)
-        time.sleep(0.05)
+        time.sleep(0.03)
     
     message5 = " Каждый момент, проведенный с тобой, становится незабываемым!"
     for char in message5:
@@ -32,3 +33,21 @@ def love_confession():
         time.sleep(0.06)
   
 love_confession()
+
+def xt(t):
+    return 16 * math.sin(t) ** 3
+def yt(t):
+    return 13 * math.cos(t) - 5 \
+    * math.cos(2 * t) - 2 * \
+    math.cos(3 * t) - math.cos(4 * t)
+t = turtle.Turtle()
+t.speed(500)
+turtle.colormode(255)
+turtle.Screen().bgcolor(0, 0, 0)
+for i in range(2550):
+    t.goto((xt(i) * 20, yt(i) * 20))
+    t.pencolor((255 - i) % 255, i % 255, (255 * i) // 2 % 255)
+    t.goto(0, 0)
+t.hideturtle()
+turtle.update()
+turtle.mainloop()
